58633240da73b4d63e17c08f86bab30a8b4265d5
liahav <8314370+liahav@users.noreply.github.com>
2026-02-02 20:34:54 +0200

    Fix header and footer

--- icepath-complete.jsx
+++ icepath-complete.jsx
@@ -1,3 +1,4 @@
+import { createRoot } from 'react-dom/client'
 import React, { useState, useMemo } from 'react';
 
 const App = () => {
@@ -1260,3 +1261,7 @@ const App = () => {
 };
 
 export default App;
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
